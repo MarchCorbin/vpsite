782a9044f0797f7518c5749e5afaac4211d9e6e7
Corbin March <marchcorbin@gmail.com>
2021-06-15 21:17:11 -0600

    Working player on the music page

--- src/components/screens/musicScreen/MusicScreen.js
+++ src/components/screens/musicScreen/MusicScreen.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 
@@ -7,21 +8,39 @@ import NavBar from '../../navBar/NavBar'
 import './MusicScreen.css'
 
 const MusicScreen = () => {
+    let player = document.querySelector('.music-player')
+    player = React.createRef()
 
+  const handleClick = (e) => {
+    let playBtn = document.querySelector('.rhap_play-pause-button')
+    let songName = document.querySelector('.current-song')
+    console.log(player)
+    player.current.audio.current.src = VPSongs[e.target.id].location
+    songName.innerText = VPSongs[e.target.id].title
+    playBtn.click()
+  }
 
+  let idnum = -1;
   return (
     <>
     <Header />
     <NavBar />
+    <div className='song-container'>
     {VPSongs.map((song) => {
+      idnum++;
       return(
-        <div className="single-song">
-          <h2>{song.title}</h2>
+        <div id={idnum} onClick={handleClick} className="single-song">
+          <h2 className="song-title">{song.title}</h2>
         </div>
       )
     })}
+    </div>
     <footer>
+      <h2 className="current-song"></h2>
     <AudioPlayer
+        autoPlayAfterSrcChange
+        ref={player}
+        src
         className="music-player"
         showFilledProgress
         showSkipControls
